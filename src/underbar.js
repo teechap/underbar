da0984ec5aec65cba7bbfbc1c4679488a02a3c4e
Travis <travisechapman@gmail.com>
2014-11-17 21:07:19 -0500

    Section 2 of underbar spec finished

--- src/underbar.js
+++ src/underbar.js
@@ -242,12 +242,13 @@ var _ = {};
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
-    if (iterator === undefined) {
-        return !_.every(collection, function(item) {
-            return !!item;
-        });
-    }
-
+    iterator = iterator || _.identity;
+    return _.reduce(collection, function(wasFound, item){
+      if (iterator(item) || wasFound) {
+        return true;
+      }
+      return false;
+    }, false);
   };
 
 
